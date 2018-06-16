f8ba0308d1a1ecb2eb22d5b205b7e6a2b4a319e0
Ibrahim Hammad <iyhammad@kanooit.onmicrosoft.com>
2018-06-16 10:35:50 +0300

    Create Minimal Solution

---
+++ WorkflowLibrary/Activities/AddWorkflowRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Abp.Dependency;
+using Abp.Domain.Uow;
+
+namespace WorkflowLibrary.Activities
+{
+
+    public sealed class AddWorkflowRequest : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            var unitOfWorkManager = IocManager.Instance.Resolve<IUnitOfWorkManager>();
+
+            // Obtain the runtime value of the Text input argument
+            string text = context.GetValue(this.Text);
+        }
+    }
+}
